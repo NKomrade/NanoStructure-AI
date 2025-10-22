669749433872a608d31a79c774521b22ed80e0a3
Nitin Kumar Singh <dear.nk.comrade@gmail.com>
2025-10-22 14:33:57 +0530

    fixed main.py

--- backend/main.py
+++ backend/main.py
@@ -67,7 +67,12 @@ app = FastAPI(
 # --- CORS Configuration ---
 origins = [
     "https://nano-structure-ai.vercel.app",
-    "http://localhost:3000"
+    "https://nanostructure-ai-backend.onrender.com",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
